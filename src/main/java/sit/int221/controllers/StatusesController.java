65d251e154b79ff265eb20adf6ebf0f8a228f785
Natsaran Sae-oueng <zhinobid@gmail.com>
2024-05-10 15:29:55 +0700

    [Update] : add delete endpoint without tranfer function

--- src/main/java/sit/int221/controllers/StatusesController.java
+++ src/main/java/sit/int221/controllers/StatusesController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import sit.int221.dtos.request.NewStatusDTO;
 import sit.int221.dtos.response.StatusDetailDTO;
+import sit.int221.dtos.response.TaskHomeDTO;
 import sit.int221.entities.Statuses;
+import sit.int221.entities.Tasks;
 import sit.int221.exceptions.ErrorResponse;
 import sit.int221.exceptions.StatusNotFoundException;
 import sit.int221.exceptions.TaskNotFoundException;
@@ -19,7 +21,9 @@ import sit.int221.services.StatusesService;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:5173","http://localhost:5174","http://localhost:80" ,"http://ip23tt1.sit.kmutt.ac.th","http://ip23tt1.sit.kmutt.ac.th:1449", "http://10.0.208.95:5173/"})
 @RestController
@@ -56,6 +60,12 @@ public class StatusesController {
     public StatusDetailDTO putStatus(@PathVariable Integer statusId, @RequestBody NewStatusDTO newStatus){
         Statuses updatedStatus = statusesService.updateStatus(statusId, newStatus);
         return modelMapper.map(updatedStatus, StatusDetailDTO.class);
+    }
+
+    @DeleteMapping("/{statusId}")
+    public Map<String, Object> deleteStatus(@PathVariable Integer statusId){
+        statusesService.removeStatus(statusId);
+        return Collections.emptyMap();
     }
 
     @ExceptionHandler(StatusNotFoundException.class)
--- src/main/java/sit/int221/services/StatusesService.java
+++ src/main/java/sit/int221/services/StatusesService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import sit.int221.dtos.request.NewStatusDTO;
 import sit.int221.entities.Statuses;
+import sit.int221.entities.Tasks;
 import sit.int221.exceptions.StatusNotFoundException;
+import sit.int221.exceptions.TaskNotFoundException;
 import sit.int221.repositories.StatusesRepository;
 
 import java.util.List;
@@ -25,7 +27,7 @@ public class StatusesService {
 
     public Statuses findStatusById(Integer statusID) {
         return statusesRepository.findById(statusID).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Task "+ statusID + " Doesn't Exist!!!"));
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Status "+ statusID + " Doesn't Exist!!!"));
     }
 
     public Statuses findStatusByName(String statusName) {
@@ -43,16 +45,13 @@ public class StatusesService {
         return statusesRepository.saveAndFlush(statuses);
     }
 
-//    public Tasks removeTask(Integer taskId) {
-//        Tasks findTasks = statusesRepository.findById(taskId).orElseThrow(
-//                () -> new TaskNotFoundException("NOT FOUND"));
-//        statusesRepository.deleteById(taskId);
-//        return findTasks;
-//    }
+    public void removeStatus(Integer statusId) {
+        findStatusById(statusId);
+        statusesRepository.deleteById(statusId);
+    }
 
     public Statuses updateStatus(Integer statusId, NewStatusDTO newStatus) {
-        Statuses findStatus = statusesRepository.findById(statusId).orElseThrow(
-                () -> new StatusNotFoundException("NOT FOUND"));
+        Statuses findStatus = findStatusById(statusId);
         if((findStatusByName(newStatus.getName()) != null) && !(findStatus.getName().equals(newStatus.getName()))){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Duplicate i sus");
         }
