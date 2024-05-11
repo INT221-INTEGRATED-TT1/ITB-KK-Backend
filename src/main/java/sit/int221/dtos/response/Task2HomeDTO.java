9939ee4eaab77449150ef25519bca45350ee4547
Natsaran Sae-oueng <zhinobid@gmail.com>
2024-05-11 20:10:02 +0700

    [Fix] : restore id response from Task2HomeDTO

--- src/main/java/sit/int221/dtos/response/Task2HomeDTO.java
+++ src/main/java/sit/int221/dtos/response/Task2HomeDTO.java
@@ -5,6 +5,7 @@ import sit.int221.entities.Statuses;
 
 @Data
 public class Task2HomeDTO {
+    private Integer id;
     private String title;
     private String assignees;
     private Statuses status;
