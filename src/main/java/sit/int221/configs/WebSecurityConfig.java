f26d3bd6b6389ce1ad52c369ba087213728bfe07
Poom <papangkorn.poomm@gmail.com>
2024-08-29 17:51:36 +0700

    [FIX]: handle auth exception, authFilter

--- src/main/java/sit/int221/configs/WebSecurityConfig.java
+++ src/main/java/sit/int221/configs/WebSecurityConfig.java
@@ -1,14 +1,22 @@
 package sit.int221.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
+import sit.int221.filters.JwtAuthFilter;
+import sit.int221.services.JwtUserDetailsService;
 
 import java.util.Collections;
 
@@ -17,22 +25,41 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+    @Autowired
+    JwtUserDetailsService jwtUserDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeRequests(authorize -> authorize
-//                        .requestMatchers("/authentications/**").permitAll()
+//                        .requestMatchers("/authentications/login").permitAll()
 //                        .requestMatchers("/v2/tasks/**").permitAll()
 //                        .requestMatchers(("/users/**")).permitAll()
 //                        .requestMatchers("/v2/statuses/**").permitAll()
 //                        .anyRequest().authenticated())
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 //                .httpBasic(withDefaults());
-                        .anyRequest().permitAll()).httpBasic(withDefaults());
+                        .anyRequest().permitAll());
         return httpSecurity.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(jwtUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
